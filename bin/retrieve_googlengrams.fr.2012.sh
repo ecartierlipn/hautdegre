5232c5ea9f0b7215914e802a002221cf8b34835e
Emmanuel Cartier <emmanuel.cartier@lipn.univ-paris13.fr>
2020-07-20 15:29:16 +0200

    update

---
+++ bin/retrieve_googlengrams.fr.2012.sh
@@ -0,0 +1,67 @@
+curl http://storage.googleapis.com/books/ngrams/books/googlebooks-fre-all-1gram-20120701-a.gz > ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-1gram-20120701-a.gz
+echo "saved ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-1gram-20120701-a.gz"
+
+curl http://storage.googleapis.com/books/ngrams/books/googlebooks-fre-all-2gram-20120701-ar.gz > ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-2gram-20120701-ar.gz
+echo "saved ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-2gram-20120701-ar.gz"
+
+curl http://storage.googleapis.com/books/ngrams/books/googlebooks-fre-all-3gram-20120701-ar.gz > ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-3gram-20120701-ar.gz
+echo "saved ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-3gram-20120701-ar.gz"
+
+curl http://storage.googleapis.com/books/ngrams/books/googlebooks-fre-all-1gram-20120701-e.gz > ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-1gram-20120701-e.gz
+echo "saved ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-1gram-20120701-e.gz"
+
+curl http://storage.googleapis.com/books/ngrams/books/googlebooks-fre-all-2gram-20120701-ex.gz > ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-2gram-20120701-ex.gz
+echo "saved ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-2gram-20120701-ex.gz"
+
+curl http://storage.googleapis.com/books/ngrams/books/googlebooks-fre-all-3gram-20120701-ex.gz > ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-3gram-20120701-ex.gz
+echo "saved ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-3gram-20120701-ex.gz"
+
+curl http://storage.googleapis.com/books/ngrams/books/googlebooks-fre-all-1gram-20120701-h.gz > ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-1gram-20120701-h.gz
+echo "saved ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-1gram-20120701-h.gz"
+
+curl http://storage.googleapis.com/books/ngrams/books/googlebooks-fre-all-2gram-20120701-hy.gz > ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-2gram-20120701-hy.gz
+echo "saved ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-2gram-20120701-hy.gz"
+
+curl http://storage.googleapis.com/books/ngrams/books/googlebooks-fre-all-3gram-20120701-hy.gz > ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-3gram-20120701-hy.gz
+echo "saved ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-3gram-20120701-hy.gz"
+
+curl http://storage.googleapis.com/books/ngrams/books/googlebooks-fre-all-1gram-20120701-m.gz > ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-1gram-20120701-m.gz
+echo "saved ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-1gram-20120701-m.gz"
+
+curl http://storage.googleapis.com/books/ngrams/books/googlebooks-fre-all-2gram-20120701-ma.gz > ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-2gram-20120701-ma.gz
+echo "saved ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-2gram-20120701-ma.gz"
+
+curl http://storage.googleapis.com/books/ngrams/books/googlebooks-fre-all-3gram-20120701-ma.gz > ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-3gram-20120701-ma.gz
+echo "saved ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-3gram-20120701-ma.gz"
+
+curl http://storage.googleapis.com/books/ngrams/books/googlebooks-fre-all-2gram-20120701-me.gz > ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-2gram-20120701-me.gz
+echo "saved ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-2gram-20120701-me.gz"
+
+curl http://storage.googleapis.com/books/ngrams/books/googlebooks-fre-all-3gram-20120701-me.gz > ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-3gram-20120701-me.gz
+echo "saved ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-3gram-20120701-me.gz"
+
+curl http://storage.googleapis.com/books/ngrams/books/googlebooks-fre-all-1gram-20120701-s.gz > ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-1gram-20120701-s.gz
+echo "saved ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-1gram-20120701-s.gz"
+
+curl http://storage.googleapis.com/books/ngrams/books/googlebooks-fre-all-2gram-20120701-su.gz > ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-2gram-20120701-su.gz
+echo "saved ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-2gram-20120701-su.gz"
+
+curl http://storage.googleapis.com/books/ngrams/books/googlebooks-fre-all-3gram-20120701-su.gz > ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-3gram-20120701-su.gz
+echo "saved ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-3gram-20120701-su.gz"
+
+curl http://storage.googleapis.com/books/ngrams/books/googlebooks-fre-all-1gram-20120701-u.gz > ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-1gram-20120701-u.gz
+echo "saved ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-1gram-20120701-u.gz"
+
+curl http://storage.googleapis.com/books/ngrams/books/googlebooks-fre-all-2gram-20120701-ul.gz > ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-2gram-20120701-ul.gz
+echo "saved ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-2gram-20120701-ul.gz"
+
+curl http://storage.googleapis.com/books/ngrams/books/googlebooks-fre-all-3gram-20120701-ul.gz > ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-3gram-20120701-ul.gz
+echo "saved ../data_ngrams/googlengrams.fr.2012/googlebooks-fre-all-3gram-20120701-ul.gz"
+
+cd ../data_ngrams/googlengrams.fr.2012
+
+for file in *.gz
+do
+  zgrep -h -i -E '^(archi|extra|hyper|hypra|maxi|méga|super|ultra)[ \w-]+(18|19|20)[0-9]{2}' $file  | grep -v '_' > $file.cleaned.csv
+  echo "$file parsed"
+done
---
+++ bin/retrieve_googlengrams.fr.2020.sh
@@ -0,0 +1,52 @@
+cd ../data_ngrams/googlengrams.fr.2020
+
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/1-00000-of-00006.gz > 1-00000-of-00006.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/1-00001-of-00006.gz > 1-00001-of-00006.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/1-00002-of-00006.gz > 1-00002-of-00006.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/1-00003-of-00006.gz > 1-00003-of-00006.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/1-00004-of-00006.gz > 1-00004-of-00006.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/1-00005-of-00006.gz > 1-00005-of-00006.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/2-00011-of-00115.gz > 2-00011-of-00115.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/2-00017-of-00115.gz > 2-00017-of-00115.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/2-00020-of-00115.gz > 2-00020-of-00115.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/2-00024-of-00115.gz > 2-00024-of-00115.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/2-00031-of-00115.gz > 2-00031-of-00115.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/2-00032-of-00115.gz > 2-00032-of-00115.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/2-00047-of-00115.gz > 2-00047-of-00115.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/2-00067-of-00115.gz > 2-00067-of-00115.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/2-00073-of-00115.gz > 2-00073-of-00115.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/2-00081-of-00115.gz > 2-00081-of-00115.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/2-00082-of-00115.gz > 2-00082-of-00115.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/2-00104-of-00115.gz > 2-00104-of-00115.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/2-00108-of-00115.gz > 2-00108-of-00115.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/3-00082-of-01197.gz > 3-00082-of-01197.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/3-00089-of-01197.gz > 3-00089-of-01197.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/3-00129-of-01197.gz > 3-00129-of-01197.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/3-00142-of-01197.gz > 3-00142-of-01197.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/3-00172-of-01197.gz > 3-00172-of-01197.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/3-00176-of-01197.gz > 3-00176-of-01197.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/3-00176-of-01197.gz > 3-00176-of-01197.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/3-00210-of-01197.gz > 3-00210-of-01197.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/3-00216-of-01197.gz > 3-00216-of-01197.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/3-00432-of-01197.gz > 3-00432-of-01197.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/3-00432-of-01197.gz > 3-00432-of-01197.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/3-00676-of-01197.gz > 3-00676-of-01197.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/3-00676-of-01197.gz > 3-00676-of-01197.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/3-00727-of-01197.gz > 3-00727-of-01197.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/3-00727-of-01197.gz > 3-00727-of-01197.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/3-00832-of-01197.gz > 3-00832-of-01197.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/3-00832-of-01197.gz > 3-00832-of-01197.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/3-00853-of-01197.gz > 3-00853-of-01197.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/3-00853-of-01197.gz > 3-00853-of-01197.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/3-01084-of-01197.gz > 3-01084-of-01197.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/3-01084-of-01197.gz > 3-01084-of-01197.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/3-01122-of-01197.gz > 3-01122-of-01197.gz
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/totalcounts-1 > totalcounts-1
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/totalcounts-2 > totalcounts-2
+curl http://storage.googleapis.com/books/ngrams/books/20200217/fre/totalcounts-3 > totalcounts-3
+
+for file in *.gz
+do
+  zgrep -h -E '^(archi|extra|hyper|hypra|maxi|méga|super|ultra)' $file > $file.cleaned.csv
+  echo "$file parsed"
+done
